8dcf6b6bb61c29b069359186bd8eeaea92acd5a5
Firas Mostafa <firassmostafa15@gmail.com>
2025-05-11 00:01:30 +0300

    Adding custom admin panal

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,41 @@
-# Register your models here.
+# Django admin customizqtion
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    # Define the admin pages for users
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {'fields': (
+                'is_active',
+                'is_superuser',
+                'is_staff',
+            )}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'name',
+                'password1',
+                'password2',
+                'is_staff',
+                'is_superuser',
+                'is_active',
+            ),
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
